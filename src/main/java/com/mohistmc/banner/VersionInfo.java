240c5b95e207007c1bd26af23dde4edb45232d78
Mgazul <mgazul@163.com>
2025-01-17 00:40:22 +0800

    Remove ci_version

--- src/main/java/com/mohistmc/banner/VersionInfo.java
+++
@@ -1,8 +0,0 @@
-package com.mohistmc.banner;
-
-/**
- * Automatically generated file containing build version information.
- */
-public final class VersionInfo {
-	public static final String VERSION = "800";
-}
