8fc3ad477b1c9f33a574218f1851fd6cbcd841f4
Mgazul <mgazul@163.com>
2025-01-17 00:28:51 +0800

    Fix build

---
+++ src/main/java/com/mohistmc/banner/VersionInfo.java
@@ -0,0 +1,8 @@
+package com.mohistmc.banner;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class VersionInfo {
+	public static final String VERSION = "800";
+}
